735b04c63485bf3d7b4f02bd92e5263e19273ab1
Hemangi Badhe <157040048+HemaBadhe@users.noreply.github.com>
2025-03-04 18:47:00 +0530

    Add files via upload

---
+++ JpaHibernateEmployees/JpaHibernateEmployees/src/main/java/com/example/JpaHibernateEmployees/JpaHibernateEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.example.JpaHibernateEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateEmployeesApplication.class, args);
+	}
+
+}

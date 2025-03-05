c43f7a6fb694014c6b750d652954012c83eea2d4
Hemangi Badhe <157040048+HemaBadhe@users.noreply.github.com>
2025-03-05 15:53:23 +0530

    Add files via upload

---
+++ HotelManagementSystem/HotelManagementSystem/src/main/java/com/example/demo/service/HotelService.java
@@ -0,0 +1,47 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.entity.Hotel;
+import com.example.demo.repository.HotelRepository;
+
+public class HotelService {
+	@Autowired
+	private HotelRepository hotelRepository;
+	
+	
+	//getting data
+	public List<Hotel> getAllHotel(){
+		return hotelRepository.findAll();
+	}
+	
+	//saving data
+	public Hotel saveHotel(Hotel hotel) {
+		return hotelRepository.save(hotel);
+	}
+	
+	//updating
+	public Hotel updateHotel(Hotel hotel) {
+		return hotelRepository.save(hotel);
+	}
+	
+	//Get hotel Id
+	public Hotel getHotelbyId(Long id) {
+		return hotelRepository.findById(id).orElse(null);
+	}
+	
+	
+	//deleting hotel by id 
+	public boolean deleteHotelById(Long id) {
+		if(hotelRepository.existsById(id)) {
+			hotelRepository.deleteById(id);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+
+}
